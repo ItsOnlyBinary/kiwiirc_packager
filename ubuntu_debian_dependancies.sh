2035602785f5ebd23b2fb7abdfd28d1d64413483
ItsOnlyBinary <github@itsonlybinary.com>
2018-12-13 14:22:18 +0000

    update to node.js 10.x.x and latest golang,

--- ubuntu_debian_dependancies.sh
+++ ubuntu_debian_dependancies.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# GoLang
+add-apt-repository -y ppa:longsleep/golang-backports
 
+# Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get update
-sudo apt-get install golang yarn nodejs ruby ruby-dev build-essential rpm libffi-dev
+# Node.js
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+
+# nodejs script performs an apt-get update after adding its source
+sudo apt-get install -y golang-go yarn nodejs ruby ruby-dev build-essential rpm libffi-dev
 
 sudo gem install fpm
